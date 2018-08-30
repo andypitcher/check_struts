463f1fa71c963bb14d95ecae3258570f8306603c
andy pitcher <ap@lbn.com>
2018-08-30 19:07:55 -0400

    script:mlocate+lsof pbk:rexel

--- check-struts.sh
+++ check-struts.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
-#check_struts: Struts lib version checker 
+#
+# Auth: a.pitcher
+# check_struts: Struts lib version checker 
+
+struts_mlocate_true=struts_mlocate_$(hostname).txt
+struts_lsof_true=struts_lsof_$(hostname).txt
+TMP_result_true=struts_lsof_$(hostname).txt
 
 print_version(){
 
 if [ ! -z "$2" ]
 then
-	echo "$1: $2"
+        echo "$1: $2"
 else
-   	echo "$1: unknown"
-fi	
+        echo "$1: unknown"
+fi
+}
+
+struts_lsof() {
+
+lsof | grep struts | awk '{print $10}' > $struts_lsof_true
+
+}
+
+struts_mlocate(){
+
+locate struts | grep jar > $struts_mlocate_true
+
 }
 
 struts_version () {
-TMP_result=struts_lsof_$(hostname).txt
-TMP_result_true=struts_versions_$(hostname).txt
-lsof | grep struts > $TMP_result
-dos2unix $TMP_result &> /dev/null
-if grep -q struts $TMP_result && grep -qv 'check-strut\|struts_lsof\|struts_versions' $TMP_result
+
+struts_lsof
+struts_mlocate
+if cat $struts_mlocate_true $struts_lsof_true | sort | uniq | grep jar > $TMP_result_true
 then
-	echo -e "Libs path and versions loaded on the system:"
-       	cat $TMP_result | awk '{print $9}' | sort | uniq -d | grep struts > $TMP_result_true
-       	for libs in $(cat $TMP_result_true)
-            do
-	     	version=$(unzip -p $libs META-INF/MANIFEST.MF | grep -F Specification-Version 2>/dev/null | awk '{print $2}')
-		print_version $libs $version
-            done
+echo -e "Libs path and versions loaded on the system:"
+for libs in $(cat $TMP_result_true)
+    do
+          version=$(unzip -p $libs META-INF/MANIFEST.MF | grep -F Specification-Version 2>/dev/null | awk '{print $2}')
+          print_version $libs $version
+    done
 else
-       echo -e "No struts lib found"
+    echo -e "No struts lib found"
+rm -rf $TMP_result_true
 fi
-rm -rf $TMP_result $TMP_result_true
 }
 struts_version
+
